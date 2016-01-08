d73aaf0fe6cb4a5f8fb216ff7b5028fc841c6ef0
Tom Bombabil <amanibhavam@destructuring.org>
2016-01-08 14:06:41 -0800

    using marginalia

--- project.clj
+++ project.clj
@@ -1,13 +1,19 @@
 (defproject cs1 "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170" :classifier "aot"
                   :exclusion [org.clojure/data.json]]
                  [org.clojure/data.json "0.2.6" :classifier "aot"]]
+
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :plugins [[lein-npm "0.6.1"]]
-  :npm {:dependencies [[source-map-support "0.3.2"]]}
+  :plugins [[lein-npm "0.6.1"]
+            [michaelblume/lein-marginalia "0.9.0"] ; forked https://github.com/MichaelBlume/lein-marginalia
+            ]
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
-  :target-path "target")
+  :target-path "target"
+
+  ; lein-npm: lein npm install, lein deps
+  :npm {:dependencies [[source-map-support "0.3.2"]]})
